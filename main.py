0c94f6ba89e4ac4432e7b493417fbd09f80bb406
dkmisterv <vilmar.neto@gmail.com>
2016-07-07 10:53:32 -0300

    Main do programa principal terminado

--- main.py
+++ main.py
@@ -68,14 +68,14 @@ def verify_entry(nomedaentrada):
         print("\nArquivo novo.\nVamos processar o arquivo pela primeira vez.\nAguarde\n")
         return False
 #####################################################
-def test():
+def open_csv():
     try:
-        handler = input("Nome do arquivo:")
-        f = open(handler + ".csv",'r',errors='ignore', encoding ="UTF-8")
-        return [f,handler]
+        file_name = input("Nome do arquivo:")
+        csv_file = open(file_name + ".csv",'r',errors='ignore', encoding ="UTF-8")
+        return [csv_file,file_name] 
     except:
         print("Arquivo inexistente.\n")
-        return test()
+        return open_csv()
 
 ####################################################
 # Main
@@ -83,7 +83,7 @@ def test():
 Registers = []
 files = []
 
-k = test()
+k = open_csv()
 
 f = k[0]
 
@@ -92,3 +92,48 @@ handler = k[1]
 del k 
 # processamento de arquivo 
 offset = 0
+
+if os.path.isfile(handler + 'Data.bin') == False:
+    with open(handler + 'Data.bin', "wb") as handler_data:
+        for line in csv.reader(f, dialect= 'excel',delimiter=''):
+            if line[0] == "Videotitle link_link":
+                del line 
+            else:
+                i = 1
+                for element in line[1]:
+                    i = i + 1
+                    if element =="":
+                        del line[i-1::]
+                newElement = Register(offset,line)
+                Registers.append(newElement)
+                pickle.dump(newElement, handler_data, pickle.HIGHEST_PROTOCOL)
+
+                offset = offset + 1
+    f.close()
+
+    make_index_files(Registers, handler)
+
+print("\nArquivo " + handler + " aberto e processado com sucesso\n")
+
+arquivo_de_saida = input("Para melhor visualizacao de dados, um arquivo txt sera criado. Digite o nome dele: ")
+arquivo_de_saida = arquivo_de_saida + ".txt"
+print(arquivo_de_saida + "criado\n")
+saida = open(arquivo_de_saida,"w")
+
+menu = 1
+while menu != 99:
+    print("""_____ ______ ________ _________ ___ ___ ________ _______ ________ ________ ________ ___ ___
+|\ _ \ _ \|\ __ \|\___ ___\\ \|\ \ |\ ____\|\ ___ \ |\ __ \|\ __ \|\ ____\|\ \|\ \
+\ \ \\\__\ \ \ \ \|\ \|___ \ \_\ \ \\\ \ \ \ \___|\ \ __/|\ \ \|\ \ \ \|\ \ \ \___|\ \ \\\ \
+\ \ \\|__| \ \ \ __ \ \ \ \ \ \ __ \ \ \_____ \ \ \_|/_\ \ __ \ \ _ _\ \ \ \ \ __ \
+\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \|____|\ \ \ \_|\ \ \ \ \ \ \ \\ \\ \ \____\ \ \ \ \
+\ \__\ \ \__\ \__\ \__\ \ \__\ \ \__\ \__\ ____\_\ \ \_______\ \__\ \__\ \__\\ _\\ \_______\ \__\ \__\
+\|__| \|__|\|__|\|__| \|__| \|__|\|__| |\_________\|_______|\|__|\|__|\|__|\|__|\|_______|\|__|\|__|
+\|______|""")
+    print ("Bem-vindo ao MATH_SEARCH\n")
+    print ("[1] - Listagem de todos videos\n")
+    print ("[2] - Busca por palavra-chave\n")
+    print ("[99] - Saida do programa\n")
+    menu=int(input())
+    switch(menu,handler,saida)
+    input()
