a25b264bc8933d7a36c23b9e3c917f2f8aef88f7
dkmisterv <vilmar.neto@gmail.com>
2016-07-06 22:46:26 -0300

    Adicionada função do nome de video

--- main.py
+++ main.py
@@ -33,3 +33,28 @@ def switch(menu, handler,exit):
         
     return 0
 #####################################################
+
+
+def video_name(name_archive, word, exit):
+    # Busca palvra consultada
+    # Imprime todos videos com a palavra consultada
+    list_vid = []
+
+    f = open(name_archive + "data_word_index.bin",'rb')
+
+    tree = pickle.load(f)
+
+    word = word.lower()
+
+    list_vid = list_video(word,tree,name_archive)
+
+    print_list(list_vid)
+
+    if list_vid != None:
+        for i in list_vid:
+            r = i.__repr__()
+            exit.write(r + "\n\n")
+
+    f.close()
+
+#####################################################
